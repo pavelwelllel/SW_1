3c19fe0f48422dc7a9f5961c173b8f15be001b2a
pavelwelllel <pavelpopov240306@gmail.com>
2025-05-05 23:00:22 +0300

    Update SW_1.h
    bugfix due im dump

--- SW_1.h
+++ SW_1.h
@@ -1,16 +1,9 @@
 #ifndef SW_1_H
 #define SW_1_H
 
-// Для size_t в C++
-#include <stddef.h>
+#include <cstddef>
 
-// Задание 1: Подсчет максимального количества одинаковых байт (исключая 0x00 и 0xFF)
-int CountSimilarBytes(long long int num);
 
-// Задание 2: Перестановка байт в unsigned short
+int CountSimilarBytes(unsigned long long num);
 void SwapBytes(unsigned short* byte);
-
-// Задание 3: Поиск байта с максимальным значением в массиве
 unsigned char FindMaxByte(const unsigned int* ar, size_t size);
-
-#endif // DZ1_H
